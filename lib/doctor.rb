ca86b6298376b6299314cc812674343a36a92744
Leah Hirschfeld <leah.hirschfeld@gmail.com>
2020-10-10 18:00:18 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class Doctor
   attr_accessor :name, :date, :patient
   @@all = []
@@ -13,6 +14,7 @@ class Doctor
   
   def new_appointment(date, patient)
     Appointment.new(date, self, patient)
+    binding.pry
   end
   
   def appointments
